3f259743766e527747f19d0b4823c48856146d2b
Eina Onting <eina@outlook.com>
2016-11-02 02:57:54 -0700

    add grid system & style examples

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,9 +5,12 @@ var Main = (props) => {
   return (
     <div>
       <Nav></Nav>
-      <h2>Main Component</h2>
-      {props.children}
-      {/*this loads the children of Main*/}
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+          {/*this loads the children of Main*/}
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
       <form onSubmit={this.onFormSubmit}>
         <input type="text" ref="location" placeholder="Enter city name"></input> <br/>
-        <button>Get Weather</button>
+        <button className="button">Get Weather</button>
       </form>
     );
   }
