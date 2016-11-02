f068e94d442c3ef8ca8e5d4195b36e2133901122
Eina Onting <eina@outlook.com>
2016-11-02 01:28:07 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var express = require('express');
 
 //create new app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+//Force it to be HTTP 
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 //tell which folder we want to serve
 app.use(express.static('public'));
 
 //start serving
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(port, function(){
+  console.log('Express server is up on port ' + PORT);
 });
